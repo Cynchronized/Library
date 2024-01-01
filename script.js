e37f234e0e8d3325cd6052b1c8602ad23856dac7
Cynchronized <austin.thieu123@gmail.com>
2024-01-01 15:22:01 -0700

    Added Modal

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
+// Declare empty array for library
 const myLibrary = [];
 
-function Book() {
 
+// Constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBooktoLibrary() {
     
 }
+
+const addBookButton = document.querySelector("[data-open-modal]")
+const submitButton = document.querySelector("[data-close-modal]")
+const modal = document.querySelector("[data-modal]")
+
+addBookButton.addEventListener("click", () => {
+    modal.showModal()
+})
+
+submitButton.addEventListener("click", () => {
+    modal.close()
+})
+
+modal.addEventListener("click", e => {
+    const dialogDimensions = modal.getBoundingClientRect()
+    if(
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+    ) {
+        modal.close()
+    }
+})
