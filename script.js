4c25accc41ad24b96c44b8c6c986c6c4a20b161a
Cynchronized <austin.thieu123@gmail.com>
2024-01-01 18:07:13 -0700

    added function to add DOM elements

--- script.js
+++ script.js
@@ -1,32 +1,98 @@
 // Declare empty array for library
-const myLibrary = [];
+const myLibrary = []
 
+// variables
+const addBookButton = document.querySelector(".new-book-btn")
+const submitButton = document.querySelector(".submit-btn")
+const modal = document.querySelector("[data-modal]")
+const booksGrid = document.querySelector('.books-grid')
+const form = document.querySelector('.add-book-form')
 
 // Constructor
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
 }
+
 
 function addBooktoLibrary() {
-    
+    const title = document.getElementById("title").value
+    const author = document.getElementById("author").value
+    const pages = document.getElementById("pages").value
+    const read = document.getElementById("read").checked
+    const newBook = new Book(title, author, pages, read)
+    myLibrary.push(newBook)
+    renderBooks()
 }
 
-const addBookButton = document.querySelector("[data-open-modal]")
-const submitButton = document.querySelector("[data-close-modal]")
-const modal = document.querySelector("[data-modal]")
+function renderBooks() {
+    resetBooksGrid()
+    for (let i = 0; i < myLibrary.length; i++) {
+        let bookItem = myLibrary[i];
+        createBookCard(bookItem);
+    }
+}
+
+function resetBooksGrid() {
+    booksGrid.innerHTML = ''
+}
+
+function createBookCard (book) {
+    const bookCard = document.createElement('div')
+    const title = document.createElement('p')
+    const author = document.createElement('p')
+    const pages = document.createElement('p')
+    const buttonGroup = document.createElement('div')
+    const readBtn = document.createElement('button')
+    const removeBtn = document.createElement('button')
+
+    bookCard.classList.add('book-card')
+    buttonGroup.classList.add('button-group')
+    readBtn.classList.add('read-btn')
+    removeBtn.classList.add('remove-btn')
+
+    title.textContent = `${book.title}`
+    author.textContent = `${book.author}`
+    pages.textContent = `${book. pages} pages`
+    removeBtn.textContent = 'Remove'
+
+    if (book.isRead) {
+        readBtn.textContent = 'Read'
+        readBtn.classList.add('btn-light-green')
+    } else {
+        readBtn.textContent = 'Not read'
+        readBtn.classList.add('btn-light-red')
+    }
+
+    bookCard.appendChild(title)
+    bookCard.appendChild(author)
+    bookCard.appendChild(pages)
+    buttonGroup.appendChild(readBtn)
+    buttonGroup.appendChild(removeBtn)
+    bookCard.appendChild(buttonGroup)
+    booksGrid.appendChild(bookCard)
+
+}
+
+
+
+form.addEventListener("submit", function() {
+    event.preventDefault();
+    addBooktoLibrary();
+    modal.close();
+})
+
+// Handle Modal
 
 addBookButton.addEventListener("click", () => {
-    modal.showModal()
+    modal.showModal();
 })
 
-submitButton.addEventListener("click", () => {
-    modal.close()
-})
 
-modal.addEventListener("click", e => {
+
+modal.addEventListener("mousedown", e => {
     const dialogDimensions = modal.getBoundingClientRect()
     if(
         e.clientX < dialogDimensions.left ||
@@ -34,6 +100,6 @@ modal.addEventListener("click", e => {
         e.clientY < dialogDimensions.top ||
         e.clientY > dialogDimensions.bottom
     ) {
-        modal.close()
+        modal.close();
     }
 })
